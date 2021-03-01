cb60f688949335114171dddfc25301cf1fdf142f
boatrain <yinger521999@gmail.com>
2021-03-01 21:27:23 +0800

    素数的相关

--- chapter8/chapter8.cpp
+++ chapter8/chapter8.cpp
@@ -59,3 +59,45 @@ int fastPow2(int a, int n) {
     }
     return res;
 }
+
+/**
+ * 判断素数
+ * @param n
+ * @return
+ */
+bool is_prime(int n) {
+    if (n <= 1)return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)return false;
+    }
+    return true;
+}
+
+/**
+ * 埃式筛法统计[2,n]中的素数个数
+ * @param n
+ * @return
+ */
+int E_sieve(int n) {
+    const int MAXN = 1e7;
+    int prime[MAXN + 1]
+    bool not_prime[MAXN + 1]
+    int k = 0;
+    for (int i = 0; i <= n; ++i) {
+        not_prime[i] = false;
+    }
+    //标记
+    for (int i = 2; i * i <= n; ++i) {
+        if (!not_prime[i]) {
+            // 2 * i可优化为 i * i
+            for (int j = 2 * i; j <= n; j += i) {
+                not_prime[j] = true;
+            }
+        }
+    }
+    //统计个数
+    for (int i = 2; i <= n; ++i) {
+        if (!not_prime[i])prime[k++] = i;
+    }
+    return k;
+}
